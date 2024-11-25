a97dbb1849f4726813a0a46dbb7864052f4c3154
Nirvan Pandagre <nirvanpandagre@Nirvans-MacBook-Air.local>
2024-11-25 19:53:40 +0530

    exploringGetPostPutDelete

--- src/app.js
+++ src/app.js
@@ -2,12 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res) =>{
-    res.send("Lets Node.js");
+// app.use("/about", (req, res) =>{
+//     res.send("I am Nirvan. I am developer.")
+// })
+
+// app.use("/",(req,res) =>{
+//     res.send("Lets Node.js");
+// })
+
+app.get("/user", (req, res) =>{
+    res.send({firstName:"Nirvan", lastName:"Pandagre"})
+} );
+
+app.post("/user", (req, res) =>{
+    res.send("Data send successfully ")
 })
 
-app.use("/about", (req, res) =>{
-    res.send("I am Nirvan. I am developer.")
+app.delete("/user",(req, res) =>{
+    res.send("Delete successfully")
 })
 
 app.listen(4000, () =>{
